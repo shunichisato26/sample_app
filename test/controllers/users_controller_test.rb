85cc5c4b10dfa02ae1236e4f6b57e8d929c6b0e0
shunichisato26 <shunichisato2468@gmail.com>
2021-05-09 19:40:37 +0900

    Add account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -64,5 +64,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
-  
+
+  # test "should not allow the not activated attribute" do
+  #   log_in_as(@non_activated_user)
+  #   assert_not @non_activated_user.activated?
+  #   get users_path
+  #   assert_select "a[href=?]", user_path(@non_activated_user), count: 0
+  #   get user_path(@non_activated_user)
+  #   assert_redirected_to root_url
+  # end  
 end
