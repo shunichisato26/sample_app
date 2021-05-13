c833427f947fe0f56f73a25cc9e081e142a6fbbe
shunichisato26 <shunichisato2468@gmail.com>
2021-05-13 19:41:16 +0900

    In the middle of chapter 13

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,13 @@ User.create!(name:  "Example User",
       activated_at: Time.zone.now)
     end
 
+    # ユーザーの一部を対象にマイクロポストを生成する
+    users = User.order(:created_at).take(6)
+    50.times do
+      content = Faker::Lorem.sentence(word_count: 5)
+      users.each { |user| user.microposts.create!(content: content) }
+    end
+
 # createの!は冪等性がある前提で書いているこのcreateの段階でfalseという結果が返ってきた場合、処理を中止する→中止しないと下の99人のfakeユーザーを作成する処理が失敗することがわかっているのに実行され、長い待ち時間が発生してしまう。
 # メソッドに!をつけるのは意図的に例外を発生させたい時に有効
 # ※冪等性→同じ操作を何度繰り返しても、同じ結果が得られるという性質。
