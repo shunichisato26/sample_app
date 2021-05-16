6eea2271518c25b28ad7263fe011e864d32e3811
shunichisato26 <shunichisato2468@gmail.com>
2021-05-17 00:23:17 +0900

    In the middle of chapter14

--- db/schema.rb
+++ db/schema.rb
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(version: 2021_05_16_090038) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"followerd_id\", \"followed_id\"", name: "index_relationships_on_followerd_id_and_followed_id", unique: true
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,33 @@ User.create!(name:  "Example User",
   activated: true,
   activated_at: Time.zone.now)
   
-  # 追加のユーザーをまとめて生成する
-  99.times do |n|
-    name  = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org"
-    password = "password"
-    User.create!(name:  name,
-      email: email,
-      password:              password,
-      password_confirmation: password,
-      activated: true,
-      activated_at: Time.zone.now)
-    end
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
+  end
 
-    # ユーザーの一部を対象にマイクロポストを生成する
-    users = User.order(:created_at).take(6)
-    50.times do
-      content = Faker::Lorem.sentence(word_count: 5)
-      users.each { |user| user.microposts.create!(content: content) }
-    end
+# ユーザーの一部を対象にマイクロポストを生成する
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# 以下のリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
 # createの!は冪等性がある前提で書いているこのcreateの段階でfalseという結果が返ってきた場合、処理を中止する→中止しないと下の99人のfakeユーザーを作成する処理が失敗することがわかっているのに実行され、長い待ち時間が発生してしまう。
 # メソッドに!をつけるのは意図的に例外を発生させたい時に有効
