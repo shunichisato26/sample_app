5e5ac6f2039c198e874aca610b4490aa7619fa42
shunichisato26 <shunichisato2468@gmail.com>
2021-05-16 15:14:58 +0900

    Add user microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,10 +4,10 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :image, content_type:  {in: %w[image/jpeg image/gif image/png],
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
                                     message: "must be a valid image format" },
                     size:         { less_than: 5.megabytes,
-                                    message: should be less than 5MB" }
+                                    message: "should be less than 5MB" }
 
   # 表示用のリサイズ済み画像を返す
   def display_image
