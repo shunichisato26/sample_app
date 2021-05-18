3c9e654a8963bb870ffa77a0f3b9d6ac861e5d23
shunichisato26 <shunichisato2468@gmail.com>
2021-05-18 19:17:34 +0900

    Add testuser into seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,6 +35,14 @@ followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
 
+# サンプルユーザーをもう1人（テスト用）
+User.create!(name:  "Test User",
+  email: "example@testuser.com",
+  password: "hogefuga",
+  password_confirmation: "hogefuga",
+  activated: true,
+  activated_at: Time.zone.now)
+
 # createの!は冪等性がある前提で書いているこのcreateの段階でfalseという結果が返ってきた場合、処理を中止する→中止しないと下の99人のfakeユーザーを作成する処理が失敗することがわかっているのに実行され、長い待ち時間が発生してしまう。
 # メソッドに!をつけるのは意図的に例外を発生させたい時に有効
 # ※冪等性→同じ操作を何度繰り返しても、同じ結果が得られるという性質。
